97f9f424969469868b05ca14bc2195badf937cff
joncomo <jon@underground.net>
2015-03-04 15:56:16 -0500

    Initial Commit

---
+++ MyPet/GameScene.h
@@ -0,0 +1,13 @@
+//
+//  GameScene.h
+//  MyPet
+//
+
+//  Copyright (c) 2015 Jon Como. All rights reserved.
+//
+
+#import <SpriteKit/SpriteKit.h>
+
+@interface GameScene : SKScene
+
+@end
---
+++ MyPet/GameViewController.h
@@ -0,0 +1,14 @@
+//
+//  GameViewController.h
+//  MyPet
+//
+
+//  Copyright (c) 2015 Jon Como. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import <SpriteKit/SpriteKit.h>
+
+@interface GameViewController : UIViewController
+
+@end
